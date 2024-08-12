97cba8f0a9c8a59a18866955a5c3908563c7c543
meet123-tech <savaliyameet15@gmail.com>
2024-08-13 01:47:43 +0530

    add Login logout functionality and add View of particular product

--- Ecomm/shop/urls.py
+++ Ecomm/shop/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
 
     path('',views.home,name='home'),
     path('about/', views.about, name='about'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('product/<int:pk>/', views.product, name='product'),
+
 
 ]
--- Ecomm/shop/views.py
+++ Ecomm/shop/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
 from .models import Product
+
 # Create your views here.
 
 
@@ -11,3 +14,33 @@ def home(request):
 
 def about(request):
     return render(request,'about.html',{})
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request,username=username,password=password )
+
+        if user is not None:
+            login(request,user)
+            messages.success(request, ('you have been logged in....'))
+            return redirect('home')
+
+        else:
+            messages.success(request, ('something went wrong Try Again'))
+            return redirect('login')
+
+    else:
+        return render(request,'login.html',{})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,('you have been logged out....'))
+    return redirect('home')
+
+
+def product(request,pk):
+    product = Product.objects.get(id=pk)
+    return render(request,'product.html',{'product':product})
