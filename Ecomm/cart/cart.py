a5cae468f0f583c65f40d8446840e53df2bc0478
meet123-tech <savaliyameet15@gmail.com>
2024-08-16 18:05:04 +0530

    show Quntities and update  Quantities of product

--- Ecomm/cart/cart.py
+++ Ecomm/cart/cart.py
@@ -12,14 +12,17 @@ class Cart():
 
         self.cart = cart
 
-    def add(self,product):
+    def add(self,product,quantity):
         product_id = str(product.id)
+        product_qunt = str(quantity)
 
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}
-    
+            # self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qunt)
+            # print(self.cart)
+        
         self.session.modified = True
 
     def get_products(self):
@@ -28,6 +31,23 @@ class Cart():
         cart_products = Product.objects.filter(id__in = product_ids)
 
         return cart_products
+    
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
+    
+
+    def update(self,product,quantity):
+        product = str(product)
+        quantity = int(quantity)
+
+        main_cart  = self.cart
+        main_cart[product] = quantity
+
+        self.session.modified = True
+
+        final_cart = self.cart
+        return final_cart
     
     
     def __len__(self):
--- Ecomm/cart/views.py
+++ Ecomm/cart/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from .cart import Cart
 from  shop.models import Product
 from django.http import JsonResponse 
+from django.contrib import messages
 
 # Create your views here.
 
@@ -10,9 +11,10 @@ def cart_summary(request):
 
     cart = Cart(request)
     products = cart.get_products
+    quantities = cart.get_quants
 
 
-    return render(request,'cart_summary.html',{'cart_products':products})
+    return render(request,'cart_summary.html',{'cart_products':products , 'prod_quantities':quantities})
 
 
 def add_cart(request):
@@ -21,11 +23,12 @@ def add_cart(request):
     
     if request.POST.get('action') == 'post':
         product_id = request.POST.get('product_id')
-        print(product_id)
+        product_qunt = request.POST.get('product_qunt')
+        # print(product_id)
         product = get_object_or_404(Product,id = product_id)
 
-        cart.add(product=product)
-
+        cart.add(product=product,quantity=product_qunt)
+        messages.success(request, f'{product.name} Added to Cart Successfully!...')
         cart_quantity = cart.__len__()
 
         response = JsonResponse({
@@ -39,7 +42,19 @@ def add_cart(request):
     pass
 
 def update_cart(request):
-    pass
+
+    cart = Cart(request)
+
+    if request.POST.get('action') == 'post':
+        product_id = request.POST.get('product_id')
+        product_qunt = request.POST.get('product_qunt')
+
+        cart.update(product=product_id,quantity=product_qunt)
+        messages.success(request,'Cart Updated Successfuly')
+        response = JsonResponse({'quantity':product_qunt})
+        return response
+        # return redirect('cart_summary')
+
 
 
 def delete_cart(request):
