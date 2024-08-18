0491bff72c4d409a1466bce24781091587ec5949
meet123-tech <savaliyameet15@gmail.com>
2024-08-18 18:14:54 +0530

    extend user profie by creating profile model and add other information to user model

--- Ecomm/shop/models.py
+++ Ecomm/shop/models.py
@@ -1,8 +1,31 @@
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 import datetime
 
 # Create your models here.
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(User,auto_now_add=True)
+    phone = models.CharField(max_length=20,blank=True)
+    address1 = models.CharField(max_length=200, blank=True)
+    address2 = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=100, blank=True)
+    state = models.CharField(max_length=100, blank=True)
+    zip_code = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+def create_profile(sender,instance,created, **kwargs):
+    if created:
+        user_profile = Profile(user = instance)
+        user_profile.save()
+
+post_save.connect(create_profile, sender=User)
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -30,6 +53,8 @@ class Customer(models.Model):
         return f"{self.first_name} {self.last_name}"
     
     
+
+
 
 
 
