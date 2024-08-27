04c5e4b78b796da05b1fb92d8bc04dcbbc6137b0
meet123-tech <savaliyameet15@gmail.com>
2024-08-27 21:45:32 +0530

    add checkout functionality

--- Ecomm/payment/urls.py
+++ Ecomm/payment/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
 
     path('payment_success', views.payment_success, name='payment_success'),
+    path('checkout', views.checkout, name='checkout'),
+    
+
     
 
 
--- Ecomm/payment/views.py
+++ Ecomm/payment/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from cart.cart import Cart
+from .models import ShippingAddress
+from .forms import ShippingForm
 
 # Create your views here.
 
 def payment_success(request):
-    return render(request, 'payment/payment_success.html')
+    return render(request, 'payment/payment_success.html',{})
+
+
+
+
+def checkout(request):
+
+    cart = Cart(request)
+    products = cart.get_products
+    quantities = cart.get_quants
+    total = cart.cart_total
+
+    
+
+    if request.user.is_authenticated:
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+        shipping_form = ShippingForm(
+            request.POST or None, instance=shipping_user)
+        return render(request,  'payment/checkout.html', {'cart_products': products , 'prod_quantities':quantities, 'totals':total, 'shipping_form':shipping_form })
+    else:
+        shipping_form = ShippingForm(
+            request.POST or None)
+        return render(request,  'payment/checkout.html', {'cart_products': products , 'prod_quantities':quantities, 'totals':total, 'shipping_form':shipping_form })
